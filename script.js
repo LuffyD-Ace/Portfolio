8ac523d46d53a3b21311e7256b5fdb1602924b3a
LuffyD-Ace <r.parabita@outlook.de>
2025-03-27 10:23:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   elements.forEach((el, index) => {
     el.style.animationDelay = `${index * 0.2}s`;
   });
+
+  // Darkmode speichern/laden
+  const toggleButton = document.getElementById("theme-toggle");
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    const mode = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    localStorage.setItem("theme", mode);
+  });
 });
